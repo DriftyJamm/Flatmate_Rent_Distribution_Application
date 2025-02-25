c154463aacab64caf864a2bdf4b93f1adc278110
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-02-25 21:44:40 +0530

    Pays_Function

---
+++ main.py
@@ -0,0 +1,27 @@
+class Bill:
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+class Flatmate:
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
+
+class pdfReport:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+    
+the_bill = Bill(amount = 120, period = "March 2021")
+john = Flatmate(name = "John", days_in_house = 20)
+marry = Flatmate(name = "Marry", days_in_house = 25)
+print("John Pays : ", john.pays(bill = the_bill, flatmate2 = marry))
+print("Marry Pays : ", marry.pays(bill = the_bill, flatmate2 = john))
