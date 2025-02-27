f4796b61c6cffe355586c8b2d41e832bb3fbaf39
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-02-27 23:06:56 +0530

    pdf design example

---
+++ fpdf_test.py
@@ -0,0 +1,10 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation = 'P', unit = 'pt', format = 'A4')
+pdf.add_page()
+
+pdf.set_font(family = 'Times', size = 24, style = 'B')
+pdf.cell(w = 300, h = 80, txt = "Flatmates Bill", border = 0, align = "c", ln = 1)
+pdf.cell(w = 200, h = 40, txt = "Period", border = 1)
+
+pdf.output("Bill.pdf")
