5047f657f1777617efbc5fccfa88b37108936a8f
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-03-03 00:00:00 +0530

    done

--- fpdf_test.py
+++ fpdf_test.py
@@ -6,5 +6,6 @@ pdf.add_page()
 pdf.set_font(family = 'Times', size = 24, style = 'B')
 pdf.cell(w = 300, h = 80, txt = "Flatmates Bill", border = 0, align = "c", ln = 1)
 pdf.cell(w = 200, h = 40, txt = "Period", border = 1)
+pdf.cell(w = 200, h = 40, txt = "Table", border = 1,ln = 1)
 
 pdf.output("Bill.pdf")
