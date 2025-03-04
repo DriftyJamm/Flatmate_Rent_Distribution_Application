86da98c756c35c6a9500ad60655d725e6f619ecd
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-03-04 21:49:17 +0530

    pdf_generator

--- fpdf_test.py
+++ fpdf_test.py
@@ -4,8 +4,8 @@ pdf = FPDF(orientation = 'P', unit = 'pt', format = 'A4')
 pdf.add_page()
 
 pdf.set_font(family = 'Times', size = 24, style = 'B')
-pdf.cell(w = 300, h = 80, txt = "Flatmates Bill", border = 0, align = "c", ln = 1)
-pdf.cell(w = 200, h = 40, txt = "Period", border = 1)
-pdf.cell(w = 200, h = 40, txt = "Table", border = 1,ln = 1)
+pdf.cell(w = 0, h = 80, txt = "Flatmates Bill", border = 1, align = "C", ln = 1)
+pdf.cell(w = 100, h = 40, txt = "Period: ", border = 1)
+pdf.cell(w = 150, h = 40, txt = "March 2021", border = 1,ln = 1)
 
 pdf.output("Bill.pdf")
